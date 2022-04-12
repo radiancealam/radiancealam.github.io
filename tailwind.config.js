863aad622c1066d1340bc2581c2bd110869b2d28
radiancealam <pratamaradiance7@gmail.com>
2022-04-12 19:57:02 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container:{
+      center:true,
+      padding: '16px',
+    },
+    extend: {
+      screens:{
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
